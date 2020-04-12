0c9c5187ae2055072faa61d99f04db16e58c76d3
Michael-Lipov <darii.lipov/michael.lipov@gmail.com>
2020-04-12 18:43:45 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[]args){
+        long ticket = 11350;
+        int stipulation = 20;
+
+        long bonusMile = ticket / stipulation;
+        System.out.println(bonusMile);
+    }
+}
